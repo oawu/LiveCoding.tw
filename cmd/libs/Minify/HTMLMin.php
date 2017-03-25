5f20e68b464ef9f9eab8f332d2964d7dbc065730
OA Wu <comdan66@gmail.com>
2017-03-25 21:07:21 +0800

    Copy from OAF2E version 3.5.2.

---
+++ cmd/libs/Minify/HTMLMin.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2016 OA Wu Design
+ */
+
+class HTMLMin {
+  public static function minify ($html) {
+    return trim (preg_replace (array ('/\>[^\S ]+/su', '/[^\S ]+\</su', '/(\s)+/su'), array ('>', '<', '\\1'), $html));
+  }
+}
