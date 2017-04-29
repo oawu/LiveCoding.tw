0ed7c55b8731e073cf257345ba030685972dd97b
OA Wu <comdan66@gmail.com>
2017-04-29 21:08:51 +0800

    Update OAF2E version 3.6.2

--- cmd/gulpfile.js
+++ cmd/gulpfile.js
@@ -77,12 +77,63 @@ gulp.task ('default', function () {
     });
   });
 });
+gulp.task ('watch', function () {
+  console.log ('\n ' + colors.red ('•') + colors.cyan (' [啟動] ') + '正在開啟 Gulp 初始化！');
+
+  read ('config_d4.rb', 'utf8', function (err, buffer) {
+    if (!buffer) return console.log (colors.red ('\n\n !!! 錯誤 !!! compass 的 ' + colors.magenta ('config_d4') + ' 檔案不見惹！\n\n'));
+
+    var path = __dirname.replace (/\\/g,'/').split (/\//);
+    if (path.length < 3) return console.log (colors.red ('\n\n !!! 錯誤 !!! 資料夾位置有誤！\n\n'));
+    path.pop ();
+    var content = buffer.replace (/http_path\s*=\s*"([\/A-Za-z0-9_-]*)"/g, 'http_path = "/' + path.pop () + '"');
+    
+    writeFile ('config.rb', content, function(err) {
+      if (err) return console.log ('\n ' + colors.red ('•') + colors.red (' [錯誤] ') + '寫入檔案失敗！');
+      else console.log ('\n ' + colors.red ('•') + colors.yellow (' [設定] ') + '設定 ' + colors.magenta ('config.rb') + ' 檔案成功！');
+
+      console.log ('\n ' + colors.red ('•') + colors.cyan (' [開啟] ') + '設定相關 ' + colors.magenta ('watch') + ' 功能！');
+
+      livereload.listen ({
+        silent: true
+      });
+
+      var watcherReload = chokidar.watch (['./root/*.html', './root/css/**/*.css', './root/js/**/*.js'], {
+        ignored: /(^|[\/\\])\../,
+        persistent: true
+      });
+
+      watcherReload.on ('change', function (path) {
+        console.log ('\n ' + colors.red ('•') + colors.yellow (' [重整] ') + '有檔案更新，檔案：' + colors.gray (path.replace (/\\/g,'/').replace (/.*\//, '')) + '');
+        gulp.start ('reload');
+        console.log ('    ' + colors.green ('reload') + ' 重新整理頁面成功！');
+      }).on ('add', function (path) {
+        console.log ('\n ' + colors.red ('•') + colors.yellow (' [重整] ') + '有新增檔案，檔案：' + colors.gray (path.replace (/\\/g,'/').replace (/.*\//, '')) + '');
+        gulp.start ('reload');
+        console.log ('    ' + colors.green ('reload') + ' 重新整理頁面成功！');
+      }).on ('unlink', function (path) {
+        console.log ('\n ' + colors.red ('•') + colors.yellow (' [重整] ') + '有檔案刪除，檔案：' + colors.gray (path.replace (/\\/g,'/').replace (/.*\//, '')) + '');
+        gulp.start ('reload');
+        console.log ('    ' + colors.green ('reload') + ' 重新整理頁面成功！');
+      });
+
+      var watcherStyle = chokidar.watch ('./root/font/icomoon/style.css', {
+        ignored: /(^|[\/\\])\../,
+        persistent: true
+      });
+
+      watcherStyle.on ('add', function (path) { gulp.start ('update_icomoon_font_icon'); })
+                  .on ('change', function (path) { gulp.start ('update_icomoon_font_icon'); });
+      // watcherStyle.on ('unlink', function (path) { gulp.start ('update_icomoon_font_icon'); });
+    });
+  });
+});
 
 // // ===================================================
 
 gulp.task ('update_icomoon_font_icon', function () {
   read ('./root/font/icomoon/style.css', 'utf8', function (err, buffer) {
-    var t = buffer.match (/\.icon-[a-zA-Z_-]*:before\s?\{\s*content:\s*"[\\A-Za-z0-9]*";\s*}/g);
+    var t = buffer.match (/\.icon-[a-zA-Z_\-0-9]*:before\s?\{\s*content:\s*"[\\A-Za-z0-9]*";\s*}/g);
       if (!(t && t.length)) return;
 
       writeFile ('./root/scss/icon.scss', '@import "_oa";\n\n@include font-face("icomoon", font-files("icomoon/fonts/icomoon.eot", "icomoon/fonts/icomoon.woff", "icomoon/fonts/icomoon.ttf", "icomoon/fonts/icomoon.svg"));\n[class^="icon-"], [class*=" icon-"] {\n  font-family: "icomoon"; speak: none; font-style: normal; font-weight: normal; font-variant: normal; text-transform: none; line-height: 1;\n  @include font-smoothing(antialiased);\n}\n\n' + t.join ('\n'), function(err) {
