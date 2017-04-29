f8b164fcc870f556ab2e0eae31083cb2a1a61276
OA Wu <comdan66@gmail.com>
2017-04-29 22:34:16 +0800

    Menu 開合

--- js/public.js
+++ js/public.js
@@ -4,5 +4,18 @@
  */
 
 $(function () {
+  // setTimeout (function () {
+  //   $('.sub').height(100);
+  // }, 2000);
+  // setTimeout (function () {
+  //   $('.sub').height(0);
+  // }, 4000);
+
+  // $('#menu .sub').each (function () {
+  //   $(this).addClass ('n' + $(this).find ('a').length);
+  // });
+  // $('#menu .group > span').click (function () {
+  //   $(this).toggleClass ('show');
+  // });
 
 });
