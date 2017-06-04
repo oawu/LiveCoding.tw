46bf64a7a9e450079479e2e3a0e727b36da41949
OA Wu <comdan66@gmail.com>
2017-06-04 22:21:06 +0800

    第二種做法 Finish!

--- js/public.js
+++ js/public.js
@@ -11,8 +11,36 @@ $(function () {
   $("#top_banner").css ({
     height: $(window).height ()
   }).find ("> div").imgLiquid ();
-  var _top_banner_now = 2;
 
+  var _length = $("#top_banner > div").length;
+  var _top_banner_now = 1;
+
+  $('#top_banner_count').text (_length);
+  $('#top_banner_now').text (_top_banner_now);
+
+  $('#top_banner_next').click (function () {
+
+    _top_banner_now = _top_banner_now + 1;
+    if (_top_banner_now > _length)
+      _top_banner_now = 1;
+    
+    $('#top_banner').attr ('class', 'n' + _top_banner_now);
+    $('#top_banner_now').text (_top_banner_now);
+  });
+  
+  $('#top_banner_prev').click (function () {
+    _top_banner_now = _top_banner_now - 1;
+    
+    if (_top_banner_now < 1)
+      _top_banner_now = $("#top_banner > div").length;
+
+    $('#top_banner').attr ('class', 'n' + _top_banner_now);
+    $('#top_banner_now').text (_top_banner_now);
+  });
+
+
+
+  // var _top_banner_now = 2;
   // $('#top_banner_count').text ($("#top_banner > div").length);
   // $('#top_banner_now').text (_top_banner_now);
 
