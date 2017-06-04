62e6d8bb5425e0886527ec90b507f38d649c5ad7
OA Wu <comdan66@gmail.com>
2017-06-04 22:02:44 +0800

    Finish top banner.

--- js/public.js
+++ js/public.js
@@ -8,7 +8,39 @@
 $(function () {
 
 
-  $('#top_banner').css ({
+  $("#top_banner").css ({
     height: $(window).height ()
-  }).imgLiquid ();
+  }).find ("> div").imgLiquid ();
+  var _top_banner_now = 2;
+
+  $('#top_banner_count').text ($("#top_banner > div").length);
+  $('#top_banner_now').text (_top_banner_now);
+
+  $('#top_banner_next').click (function () {
+    var $tmp = $('#top_banner > div').first ().clone (true);
+    $('#top_banner').append ($tmp);
+    $('#top_banner > div').first ().remove ();
+
+    _top_banner_now = _top_banner_now + 1;
+    if (_top_banner_now > $("#top_banner > div").length)
+      _top_banner_now = 1;
+
+    $('#top_banner_now').text (_top_banner_now);
+  });
+
+  $('#top_banner_prev').click (function () {
+    var $tmp = $('#top_banner > div').last ().clone (true);
+    $('#top_banner').prepend ($tmp);
+    $('#top_banner > div').last ().remove ();
+    
+    _top_banner_now = _top_banner_now - 1;
+    if (_top_banner_now < 1)
+      _top_banner_now = $("#top_banner > div").length;
+    $('#top_banner_now').text (_top_banner_now);
+  }).click ();
+
+  setTimeout (function () {
+    $('#top_banner').addClass ('transi');
+  }, 500);
+
 });
